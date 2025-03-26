1a6b05189f637976007bc3c323ef2d7738276d7f
carlos@liquidworks.com.br <carlos@liquidworks.com.br>
2025-03-26 16:32:22 -0300

    Add: CustomException

---
+++ src/main/java/com/gym/domain/exceptions/CustomException.java
@@ -0,0 +1,14 @@
+package com.gym.domain.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final int statusCode;
+
+    public CustomException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
