5f96139801a8ee31b9f968729d9ec6b3e5f02d20
carlos@liquidworks.com.br <carlos@liquidworks.com.br>
2025-03-27 16:31:25 -0300

    Add: métodos para buscar e listar clientes

--- src/main/java/com/gym/infra/repositories/CustomerRepository.java
+++ src/main/java/com/gym/infra/repositories/CustomerRepository.java
@@ -3,5 +3,9 @@ package com.gym.infra.repositories;
 import com.gym.domain.entities.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+
+    Optional<CustomerEntity> findByDocument(String document);
 }
