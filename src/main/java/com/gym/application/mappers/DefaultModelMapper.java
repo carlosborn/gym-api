fcf7904ee946855792d8e80eae78415ca36b4956
carlos@liquidworks.com.br <carlos@liquidworks.com.br>
2025-03-27 16:31:37 -0300

    Add: um mapper personalizado

---
+++ src/main/java/com/gym/application/mappers/DefaultModelMapper.java
@@ -0,0 +1,33 @@
+package com.gym.application.mappers;
+
+import com.gym.application.dtos.AddressODTO;
+import com.gym.application.dtos.CustomerODTO;
+import com.gym.application.dtos.UserSessionODTO;
+import com.gym.domain.entities.AddressEntity;
+import com.gym.domain.entities.CustomerEntity;
+import com.gym.domain.entities.UserSessionEntity;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+
+public class DefaultModelMapper extends ModelMapper {
+
+    public DefaultModelMapper(){
+        super();
+        this.initPropertiesMappers();
+    }
+
+    private void initPropertiesMappers(){
+        this.addMappings(new PropertyMap<UserSessionEntity, UserSessionODTO>() {
+            protected void configure() {
+                map(source.getUser(), destination.getUserODTO());
+            }
+        });
+
+        this.addMappings(new PropertyMap<CustomerEntity, CustomerODTO>() {
+            protected void configure() {
+                map(source.getAddress(), destination.getAddress());
+            }
+        });
+    }
+
+}
