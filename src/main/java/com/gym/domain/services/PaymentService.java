e983062707757697787cc8cef26533e3f1d0027d
carlos@liquidworks.com.br <carlos@liquidworks.com.br>
2025-03-28 14:31:41 -0300

    Add: serviço de dominio dos pagamentos

---
+++ src/main/java/com/gym/domain/services/PaymentService.java
@@ -0,0 +1,30 @@
+package com.gym.domain.services;
+
+import com.gym.domain.entities.EnrollmentEntity;
+import com.gym.domain.entities.PaymentEntity;
+import com.gym.domain.entities.PaymentStatus;
+import com.gym.infra.repositories.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    public PaymentEntity save(PaymentEntity paymentEntity) {
+        return this.paymentRepository.save(paymentEntity);
+    }
+
+    public List<PaymentEntity> findByEnrollment(EnrollmentEntity enrollmentEntity) {
+        return this.paymentRepository.findByEnrollment(enrollmentEntity);
+    }
+
+    public List<PaymentEntity> findByEnrollmentAndStatus(EnrollmentEntity enrollmentEntity, PaymentStatus status) {
+        return this.paymentRepository.findByEnrollmentAndStatus(enrollmentEntity, status);
+    }
+
+}
